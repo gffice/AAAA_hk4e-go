edc5c7ecb4c4781ec8e86a8c56831f2bd44f1794
Game Office <114347535+gffice@users.noreply.github.com>
2024-03-14 22:55:20 +0800

    Update constant_controller.go

--- dispatch/controller/constant_controller.go
+++ dispatch/controller/constant_controller.go
@@ -33,7 +33,8 @@ func (c *Controller) check(ctx *gin.Context) {
 	} else {
 		// Android
 		// _, _ = ctx.Writer.WriteString("{\"retcode\":0,\"message\":\"OK\",\"data\":{\"id\":\"2b35f1421d4a4c7c9183184c6190027e\",\"action\":\"ACTION_GEETEST\",\"geetest\":{\"challenge\":\"616018607b6940f52fbd349004038686\",\"gt\":\"16bddce04c7385dbb7282778c29bba3e\",\"new_captcha\":1,\"success\":1}}}")
-		_, _ = ctx.Writer.WriteString("{\"retcode\":0,\"message\":\"OK\",\"data\":{\"id\":\"none\",\"action\":\"ACTION_NONE\",\"geetest\":null}}")
+		//_, _ = ctx.Writer.WriteString("{\"retcode\":0,\"message\":\"OK\",\"data\":{\"id\":\"none\",\"action\":\"ACTION_NONE\",\"geetest\":null}}")
+		_, _ = ctx.Writer.WriteString("{\"retcode\":0,\"message\":\"OK\",\"data\":{\"id\":\"2b35f1421d4a4c7c9183184c6190027e\",\"action\":\"ACTION_GEETEST\",\"geetest\":{\"challenge\":\"\",\"gt\":\"\",\"new_captcha\":0,\"success\":1}}}")
 	}
 }
 
